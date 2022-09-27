fffeae145a0923f52307bb8218e8e2905bb49d8d
fridoon <fnajmullah@yahoo.com>
2022-09-27 09:25:54 +0300

    initial commit

---
+++ src/Health.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Health {
+    public static void main(String[] args){
+        System.out.println("This is Fridoon and Now is:");
+        System.out.println(new Date());
+    }
+}
