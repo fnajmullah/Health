1b15c8029c5a25bb38b8da62dd623e71ca9582fe
fridoon <fnajmullah@yahoo.com>
2022-09-27 10:22:23 +0300

    exercise 1 and 2 are complete

--- src/Health.java
+++ src/Health.java
@@ -2,7 +2,18 @@ import java.util.Date;
 
 public class Health {
     public static void main(String[] args){
-        System.out.println("This is Fridoon and Now is:");
-        System.out.println(new Date());
+        System.out.println("This is Fridoon and Date & Time is:");
+
+        // Exercise 1
+        int i=0;
+        char c='c';
+        System.out.println(i+"<default values not printed only valid if initialized>"+c);
+
+        // Exercise 2
+        System.out.println("HelloDate:"+ new Date());
+
+        // Exercise 3
+
+
     }
 }
